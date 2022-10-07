2efd44a08919d3c77f2af5b7dbb4a574aee0ce89
andylnkd <dhalwani1@gmail.com>
2022-10-07 11:31:21 -0700

    got sendgrid working

--- config/plugins.js
+++ config/plugins.js
@@ -1,29 +1,30 @@
 // path: ./config/plugins.js
 
 module.exports = ({ env }) => ({
-    email: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'admin@getmagic.ai',
-          defaultReplyTo: 'admin@getmagic.ai',
-        },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
       },
-    upload: {
-      config: {
-        provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
-        providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            Bucket: env('AWS_BUCKET'),
-          },
+      settings: {
+        defaultFrom: "admin@getmagic.ai",
+        defaultReplyTo: "admin@getmagic.ai",
+      },
+    },
+  },
+
+  upload: {
+    config: {
+      provider: "aws-s3", // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
         },
       },
     },
-
-  });
-  
+  },
+});
