b2018227f2d6c437623091de93cb2a94baacb1b3
andylnkd <dhalwani1@gmail.com>
2022-10-06 20:01:35 -0700

    testing s3 some more

--- config/plugins.js
+++ config/plugins.js
@@ -6,14 +6,15 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
         providerOptions: {
-          accessKeyId: env('AKIAY4Q32CNGFFCR747U'),
-          secretAccessKey: env('qKpFnD1oXoJNDOLM6IJi3JAnN+zTzaVKTNLE2g2v'),
-          region: env('US West (N. California) us-west-1'),
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
           params: {
-            Bucket: env('strapiv1storage'),
+            Bucket: env('AWS_BUCKET'),
           },
         },
       },
     },
     // ..
   });
+  
